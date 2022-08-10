ba5b3409a9e9f9b24704a8e19f2f75544a7db4b5
Rentaro Ishihara <asahi.online.2021@gmail.com>
2022-08-10 12:19:23 +0900

    色々更新

---
+++ js/getposts.js
@@ -0,0 +1,3 @@
+function getpostsname(){
+    const path="posts/";
+}
---
+++ js/header.js
@@ -0,0 +1,17 @@
+var headerH = $("#header").outerHeight(true);//headerの高さを取得    
+
+//スクロール途中からヘッダーの高さを変化させるための設定を関数でまとめる
+function FixedAnime() {
+	//ヘッダーの高さを取得
+	var scroll = $(window).scrollTop();
+	if (scroll >= headerH){//ヘッダーの高さを超えたら
+        $('#header').addClass('HeightMin');//#headerについているHeightMinというクラス名を付与
+	}else{
+        $('#header').removeClass('HeightMin');//HeightMinというクラス名を除去
+	}    
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	FixedAnime();//スクロール途中からヘッダーの高さを変化させる関数を呼ぶ
+});
